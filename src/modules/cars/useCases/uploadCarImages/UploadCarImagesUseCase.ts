d361a48eb47ef2b86c62a3e4f71a8a28724f5851
Douglas <douglas.hfonseca@gmail.com>
2021-06-11 22:22:51 -0300

    refatorando get user e implementando class-transformer

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { AppError } from "@shared/errors/AppError";
 import { deleteFile } from "@utils/file";
 
@@ -13,19 +14,26 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     const carExists = await this.carsImagesRepository.findByIdCar(car_id);
     if (carExists) {
       await this.carsImagesRepository.deleteCarImages(car_id);
       images_name.map(async (image) => {
+        await this.storageProvider.delete(image, "cars");
+      });
+      images_name.map(async (image) => {
         await this.carsImagesRepository.create(car_id, image);
+        await this.storageProvider.save(image, "cars");
         await deleteFile(`./tmp/cars/${image}`);
       });
     } else {
       images_name.map(async (image) => {
         await this.carsImagesRepository.create(car_id, image);
+        await this.storageProvider.save(image, "cars");
         await deleteFile(`./tmp/cars/${image}`);
       });
     }
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserControler";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -12,9 +13,11 @@ const usersRoutes = Router();
 const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
+const profileUserController = new ProfileUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.get("/", ensureAuthenticated, profileUserController.handle);
 usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
